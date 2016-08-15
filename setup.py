b6d70ffb7002a3d1297466a10efdd9adc47ac2e0
jamshid <jamshu.mkd@gmail.com>
2016-08-15 10:26:23 +0400

    up setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import py2exe, sys, os
 sys.argv.append('py2exe')
 
 setup(
-    options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
+  
     windows = [{'script': "main.py"}],
     zipfile = None,
 )
